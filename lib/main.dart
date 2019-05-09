80022c50369bd08453f43bd538b46cebda3558f8
SimonIT <simonit.orig@gmail.com>
2019-05-09 22:41:22 +0200

    barBackgroundColor bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyAppState extends State<MyApp> {
       theme: CupertinoThemeData(
         primaryColor: Color.fromRGBO(185, 213, 222, 1),
         primaryContrastingColor: Color.fromRGBO(0, 59, 78, 1),
-        // barBackgroundColor: Color.fromRGBO(0, 59, 78, 1), // closes keyboard immediately
+        barBackgroundColor: Color.fromRGBO(0, 59, 78, 1),
         textTheme: CupertinoTextThemeData(
           textStyle: TextStyle(
             color: CupertinoColors.white,
