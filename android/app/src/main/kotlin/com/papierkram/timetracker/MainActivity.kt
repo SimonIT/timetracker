57bce232793eeaa6fb7774b062103c3e0f136a81
SimonIT <simonit.orig@gmail.com>
2020-08-18 15:10:42 +0200

    Project upgrade

--- android/app/src/main/kotlin/com/papierkram/timetracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/papierkram/timetracker/MainActivity.kt
@@ -1,35 +1,5 @@
 package com.papierkram.timetracker
 
-import android.os.Bundle
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
+import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val flutter_native_splash = true
-        var originalStatusBarColor = 0
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            originalStatusBarColor = window.statusBarColor
-            window.statusBarColor = 0xffa9c4cd.toInt()
-        }
-        val originalStatusBarColorFinal = originalStatusBarColor
-
-        GeneratedPluginRegistrant.registerWith(this)
-        val vto = flutterView.viewTreeObserver
-        vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    window.statusBarColor = originalStatusBarColorFinal
-                }
-            }
-        })
-
-    }
-}
+class MainActivity : FlutterActivity()
