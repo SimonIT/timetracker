72c7ca31be7357b2fb7922620f86bf3a60970caa
SimonIT <simonit.orig@gmail.com>
2020-08-18 20:39:10 +0200

    Fix stupid flutter androidx bug https://github.com/flutter/flutter/issues/58479

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -1,8 +1,2 @@
-## Flutter wrapper
--keep class io.flutter.app.** { *; }
--keep class io.flutter.plugin.**  { *; }
--keep class io.flutter.util.**  { *; }
--keep class io.flutter.view.**  { *; }
--keep class io.flutter.**  { *; }
--keep class io.flutter.plugins.**  { *; }
--dontwarn io.flutter.embedding.**
+## https://github.com/flutter/flutter/issues/58479
+-keep class androidx.lifecycle.** { *; }
--- android/app/src/main/kotlin/com/papierkram/timetracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/papierkram/timetracker/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.papierkram.timetracker
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
